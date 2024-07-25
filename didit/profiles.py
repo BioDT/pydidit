c927c0da376619f2831e31870b8fc4c205c7375a
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2024-07-25 09:08:32 +0100

    Add schemas for metadata validation

---
+++ didit/profiles.py
@@ -0,0 +1,38 @@
+# Description: This file contains the schema for the selected level of workflow run RO-Crate
+# Please note - these are not schemas for validating the entire RO-Crate, but only that the user space input in the task file is correct
+
+from schema import Schema, And, Use, Optional, SchemaError
+
+process_run = Schema({
+    'crate_profile': And(Use(str)),
+    'agent': {
+        'name': And(Use(str)),
+        'affiliation': And(Use(str)),
+        'orcid_url': And(Use(str)),
+    }
+})
+
+workflow_run = Schema({
+    'crate_profile': And(Use(str)),
+    'agent': {
+        'name': And(Use(str)),
+        'affiliation': And(Use(str)),
+        'orcid_url': And(Use(str)),
+    }
+})
+
+provenance_run = Schema({
+    'crate_profile': And(Use(str)),
+    'agent': {
+        'name': And(Use(str)),
+        'affiliation': And(Use(str)),
+        'orcid_url': And(Use(str)),
+    }
+})
+
+
+profiles = {
+    'process-run': process_run,
+    'workflow-run': workflow_run,
+    'provenance-run': provenance_run
+}
